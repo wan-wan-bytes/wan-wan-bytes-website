7c6e42d946332a99fe4d9ebbc3241bde47dcaaa7
Robert Parker <diaboloshogunate@gmail.com>
2022-09-02 18:30:48 -0700

    add temp home page

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
-    #[Route('/home/page', name: 'app_home_page')]
+    #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
+        return $this->render('home_page/index.html.twig', []);
     }
 }
